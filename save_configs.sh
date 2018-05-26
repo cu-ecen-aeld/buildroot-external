a1221a763a1fa66a1fda237dbc0436e43179acf8
Dan Walkes <danwalkes@trellis-logic.com>
2018-05-26 22:08:32 +0000

    Add linux kernel defconfig

--- save_configs.sh
+++ save_configs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # TODO: add all other configuration save commands here for any other modified configs
 make -C buildroot savedefconfig BR2_DEFCONFIG=../base_external/configs/x86_64_qemu_defconfig
+make -C buildroot linux-update-defconfig
