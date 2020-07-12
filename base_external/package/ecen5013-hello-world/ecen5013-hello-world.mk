3c15ec59e0126937e42689aa875d8e140b4bab4b
Dan Walkes <daniel.walkes@colorado.edu>
2020-07-12 14:22:37 -0600

    Update ecen5013-hello-world.mk

--- base_external/package/ecen5013-hello-world/ecen5013-hello-world.mk
+++ base_external/package/ecen5013-hello-world/ecen5013-hello-world.mk
@@ -5,6 +5,8 @@
 ################################################################################
 
 ECEN5013_HELLO_WORLD_VERSION = 452e6e18d1323df20a309e1e55300063d893777e
+# Note: we use an https URL here because the repository is public
+# If it were private, we'd use an SSH URL instead.
 ECEN5013_HELLO_WORLD_SITE = https://github.com/cu-ecen-5013/ecen5013-hello-world.git
 ECEN5013_HELLO_WORLD_SITE_METHOD = git
 
